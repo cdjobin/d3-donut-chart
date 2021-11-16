56c5f71fec815e1ff8ae93b8374958fc333abadb
cdjobin <92936363+cdjobin@users.noreply.github.com>
2021-11-16 16:52:52 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 var width = 600,
     height = 600,
-    margin = 100,
+    margin = 50,
     radius = Math.min(width, height) / 2 - margin;
 
 var color = d3.scale.category20();
 
 var pie = d3.layout.pie()
-    .value(function (d) { return d.count; })
+    .value(function (d) { return d.value; })
     .sort(null);
 
 var arc = d3.svg.arc()
@@ -22,40 +22,71 @@ var svg = d3.select(".donut-chart")
     .attr("width", width)
     .attr("height", height)
     .append("g")
-    .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
+    .attr("transform", "translate(" + (width / 2 - (margin * 2)) + "," + height / 2 + ")");
 
-svg.append("g")
-    .attr("class", "slices");
-
-svg.append("g")
-    .attr("class", "labels");
-
-svg.append("g")
-    .attr("class", "lines");
+svg.append("g").attr("class", "slices");
+svg.append("g").attr("class", "legend").attr("transform", "translate(" + (width / 2 - (margin * 1.5)) + ",-" + margin + ")");
 
 var path = svg.select(".slices").selectAll("path");
-var polyline = svg.select(".lines").selectAll("allPolylines");
-var labels = svg.select(".labels").selectAll("allLabels");
+
 
 d3.csv("http://workzone/WET4/development/qtt972/d3-visualisation/tbl01-en.csv", type, function (error, data) {
+    if (error) throw error;
+
+    var table = d3.select('.chart-table')
+        .append('table')
+        .attr("class", "table table-condensed table-hover");
+
+    console.log(d3.keys(data[0]));
+    var titles = d3.keys(data[0]);
+
+    var headers = table.append('thead')
+        .append('tr')
+        .selectAll('th')
+        .data(titles).enter()
+        .append('th')
+        .text(function (d) {
+            return d;
+        });
+        
+    var rows = table.append('tbody')
+        .selectAll('tr')
+        .data(data).enter()
+        .append('tr');
+
+    rows.selectAll('td')
+        .data(function (d) {
+            return titles.map(function (k) {
+                return { 'value': d[k], 'name': k};
+        });
+    }).enter()
+    .append('td')
+    .attr('data-th', function (d) {
+        return d.name;
+    })
+    .text(function (d) {
+        return d.value;
+    });
+
+
     var newData = [];
     data.forEach(function (d) {
-        newData.push({ provinceTerritory: d.provinceTerritory, salary: "$45,916 or less", count: d.amt1 });
-        newData.push({ provinceTerritory: d.provinceTerritory, salary: "$45,917 - $91,831", count: d.amt2 });
-        newData.push({ provinceTerritory: d.provinceTerritory, salary: "$91,832 - $142,353", count: d.amt3 });
-        newData.push({ provinceTerritory: d.provinceTerritory, salary: "$142,354 - $202,800", count: d.amt4 });
-        newData.push({ provinceTerritory: d.provinceTerritory, salary: "$202,801 or more", count: d.amt5 });
+        newData.push({ name: d["Province or Territory"], category: "$45,916 or less", value: d["$45,916 or less"] });
+        newData.push({ name: d["Province or Territory"], category: "$45,917 - $91,831", value: d["$45,917 - $91,831"] });
+        newData.push({ name: d["Province or Territory"], category: "$91,832 - $142,353", value: d["$91,832 - $142,353"] });
+        newData.push({ name: d["Province or Territory"], category: "$142,354 - $202,800", value: d["$142,354 - $202,800"] });
+        newData.push({ name: d["Province or Territory"], category: "$202,801 or more", value: d["$202,801 or more"] });
     });
     data = newData;
 
-    var salaryByRegion = d3.nest()
-        .key(function (d) { return d.provinceTerritory; })
+    var categoryByName = d3.nest()
+        .key(function (d) { return d.name; })
         .entries(data);
 
-    //console.log(salaryByRegion);
+    //console.log(categoryByName);
 
     var label = d3.select("form").selectAll("div")
-        .data(salaryByRegion)
+        .data(categoryByName)
         .enter().append("div")
         .attr("class", "checkbox").append("label");
 
@@ -64,28 +95,15 @@ d3.csv("http://workzone/WET4/development/qtt972/d3-visualisation/tbl01-en.csv", 
         .attr("name", "provinceTerritory")
         .attr("value", function (d) { return d.key; })
         .on("change", updateGraph)
-        // JC --> ce n'est pas nécessaire d'afficher le graphique immédiatement pour chaque élément.
-        //.each(updateGraph)
-        // JC --> La ligne ci-dessous fait en sorte que toutes les cases seront cochées en affichage initial
         .property("checked", true);
-        // JC --> A quoi sert la ligne ci-dessous? Je l'ai ajoutée/supprimée et je n'ai pas vu de différence dans l'affichage
-        //				Si tu voulais t'en servir pour identifier les cases qui sont cochées, ça ne marche pas. 
-        //				Si c'est possible de le faire, il faudrait lire en détail la documentation et regarder sur les Internets pour identifier comment faire.
-        //				J'ai créé un fonction pour vérifier les cases cochées et récupérer seulement les éléments sélectionnés.
-        //.filter(function (d, i) { return !i; })
         
     label.append("span")
         .text(function (d) { return d.key; });
 
+
     // Create graph in screen
     updateGraph();
 
-    /**
-    * JC --> Ceci est mon début de fonction qui sert à afficher les résultats dans le graphique
-    * En résumé, il faut avant tout récupérer les éléments qui sont sélectionnés par les cases à cocher. 
-    * Le reste n'est pas important dans le graphique.
-    * Autrement dit, on refaire un array de valeur mais contenant seulement les valeurs sélectionnés.
-    */
     function updateGraph() {
         // Variable that will contain selected provinces/territories
         var selectedDataSet = [];
@@ -96,67 +114,48 @@ d3.csv("http://workzone/WET4/development/qtt972/d3-visualisation/tbl01-en.csv", 
             // If the checkbox is selected
             if (cb.property("checked")) {
                 // Gather information selected with checkboxes
-                for (i = 0; i < salaryByRegion.length; i++) {
-                    if (salaryByRegion[i].key == cb.property("value")) {
-                        selectedDataSet.push(salaryByRegion[i]);
+                for (i = 0; i < categoryByName.length; i++) {
+                    if (categoryByName[i].key == cb.property("value")) {
+                        selectedDataSet.push(categoryByName[i]);
                     }
                 }
             }
         });
-        // console.log(selectedDataSet);
+        //console.log(selectedDataSet);
         
-        /**
-        * JC --> Il faut maintenant faire une somme de ce qui est dans le nouveau array "selectedDataSet".
-        * Et ensuite créer le graphique
-        * Mais je ne suis pas assez fort avec D3.
-        * Autrement dit, il faut arriver à faire une structure de données qui ressemblerait à ca, c'est à dire un tableau avec les totaux:
-        [
-            { salary: "$45,916 or less", count: TOTAL_DES_CASES_COCHÉES },
-            { salary: "$45,917 - $91,831", count: TOTAL_DES_CASES_COCHÉES },
-            { salary: "$91,832 - $142,353", count: TOTAL_DES_CASES_COCHÉES },
-            { salary: "$142,354 - $202,800", count: TOTAL_DES_CASES_COCHÉES },
-            { salary: "$202,801 or more", count: TOTAL_DES_CASES_COCHÉES }
-        ]
-        *	Je sais que D3 contient déjà des fonctions de SUM mais je ne sais pas si on peut les passer directement en paramètre ou s'il faut refaire un nouveau
-        * array... 
-        *
-        */
         var newDataSet = [];
         selectedDataSet.forEach(function(d) {
             d.values.forEach(function(e) {
-                newDataSet.push({salary: e.salary, count: e.count});
+                newDataSet.push({category: e.category, value: e.value});
             });
         });
-        // console.log(newDataSet);
+        //console.log(newDataSet);
 
         var r = {};
 
         newDataSet.forEach(function(d) {
-            r[d.salary] = (r[d.salary] || 0) + parseInt(d.count);
+            r[d.category] = (r[d.category] || 0) + parseInt(d.value);
         });
 
         var result = Object.keys(r).map(function(k){
-            return { salary: k, count: r[k] }
+            return { category: k, value: r[k] }
         });
 
-        // console.log(result);
+        //console.log(result);
         
         var data0 = path.data(),
             data1 = pie(result);
 
-        // console.log(result);
+        //console.log(result);
 
         path = path.data(data1, key);
-        polyline = polyline.data(data1, key);
-        labels = labels.data(data1, key);
         
-        /*------- Pie Slice ------- */ 
         path.enter().append("path")
             .each(function (d, i) { this._current = findNeighborArc(i, data0, data1, key) || d; })
-            .attr("fill", function (d) { return color(d.data.salary); })
+            .attr("fill", function (d) { return color(d.data.category); })
             .attr("stroke", "white")
             .append("title")
-            .text(function (d) { return d.data.salary; });
+            .text(function (d) { return d.data.category; });
         
         path.exit()
             .datum(function (d, i) { return findNeighborArc(i, data1, data0, key) || d; })
@@ -168,57 +167,42 @@ d3.csv("http://workzone/WET4/development/qtt972/d3-visualisation/tbl01-en.csv", 
         path.transition()
             .duration(750)
             .attrTween("d", arcTween);
-        
-        /*------- Polylines------- */ 
-        polyline.enter().append("polyline")
-            .each(function (d, i) { this._current = findNeighborArc(i, data0, data1, key) || d; })
-            .attr("stroke", "black")
-            .style("fill", "none")
-            .attr("stroke-width", 1)
-            .attr("points", linePoints);
-        
-        polyline.exit()
-            .datum(function (d, i) { return findNeighborArc(i, data1, data0, key) || d; })
-            .transition()
-            .duration(750)
-            .attrTween("points", arcTweenPoints)
-            .remove();
-        
-        polyline.transition()
-            .duration(750)
-            .attrTween("points", arcTweenPoints);
-        
-         /*------- Labels------- */ 
-        labels.enter().append("text")
-            .each(function (d, i) {
-                this._current = findNeighborArc(i, data0, data1, key) || d;
-            })
-            .text(function (d) { return d.data.salary; })
-            .attr("transform", labelsTransform)
-            .style("text-anchor", labelsAnchor)
-            .append("span");
-        
-        labels.exit()
-            .datum(function (d, i) { return findNeighborArc(i, data1, data0, key) || d; })
-            .transition()
-            .duration(750)
-            .attrTween("transform", arcTweenLabels)
-            .styleTween("text-anchor", styleTweenLabels)
-            .remove();
 
-        labels.transition()
-            .attrTween("transform", arcTweenLabels)
-            .styleTween("text-anchor", styleTweenLabels)
-            .duration(750);
+        /* We append the legend with the current information */
+        var legend = svg.select(".legend")
+            .selectAll('.legend-entry')
+            .data(result)
+            .enter()
+            .append('g')
+            .attr('class', 'legend-entry')
+            .attr("transform", function(d, i) { return "translate(0," + i * 25 + ")"; });
+
+        legend.append('rect')
+            .attr('class', 'legend-rect')
+            .attr('width', 18)
+            .attr('height', 18)
+            .attr('fill', function (d) {
+                return color(d.category);
+            });
+        
+        legend.append('text')
+            .attr('class', 'legend-text')
+            .attr('x', 25)
+            .attr('y', 10)
+            .attr("dy", "0.35em")
+            .text(function (d) {
+                return d.category;
+            });
+        
     }
 });
 
 function key(d) {
-    return d.data.salary;
+    return d.data.category;
 }
 
 function type(d) {
-    d.count = +d.count;
+    d.value = +d.value;
     return d;
 }
 
@@ -255,94 +239,8 @@ function midAngle(d) {
     return d.startAngle + (d.endAngle - d.startAngle) / 2;
 }
 
-function linePoints(d) {
-    var posA = arc.centroid(d);
-    var posB = outerArc.centroid(d);
-    var posC = outerArc.centroid(d);
-    posC[0] = radius * 0.9 * (midAngle(d) < Math.PI ? 1 : -1);
-
-    return [posA, posB, posC];
-}
-
-function labelsTransform(d) {
-    var pos = outerArc.centroid(d);
-    pos[0] = radius * 0.99 * (midAngle(d) < Math.PI ? 1 : -1);
-
-    return 'translate(' + pos + ')';
-}
-
-function labelsAnchor(d) {
-    return (midAngle(d) < Math.PI ? 'start' : 'end');
-}
-
 function arcTween(d) {
     var i = d3.interpolate(this._current, d);
     this._current = i(0);
     return function (t) { return arc(i(t)); };
-}
-
-function arcTweenPoints(d) {
-    this._current = this._current;
-    var interpolate = d3.interpolate(this._current, d);
-    var _this = this;
-    return function (t) {
-        var d2 = interpolate(t);
-        _this._current = d2;
-        var pos = outerArc.centroid(d2);
-        pos[0] = radius * 0.95 * (midAngle(d2) < Math.PI ? 1 : -1);
-        return [arc.centroid(d2), outerArc.centroid(d2), pos];
-    };
-}
-
-function arcTweenLabels(d, i) {
-    var interpolate = d3.interpolate(this._current, d);
-    var _this = this;
-
-    var array = [];
-    var current = array[i];
-    var length = array.length;
-    var prev = array[(i+length-1) % length];
-
-    array.push[_this];
-
-    return function (t) {
-        var d2 = interpolate(t);
-        _this._current = d2;
-
-        var thisbb = _this.getBoundingClientRect();
-        console.log(array);
-
-
-        //var text = document.getElementsByTagName("text");
-        //prev = text[i].previousSibling;
-        //var prevbb = prev.getBoundingClientRect();
-
-        var pos = outerArc.centroid(d2);
-        pos[0] = radius * (midAngle(d2) < Math.PI ? 1 : -1);
-
-
-        /*if(i > 0) {
-            if ((prevbb.bottom - thisbb.bottom) <= thisbb.height) {
-                    //console.log('Début');
-                    //console.log(text[i]);
-                    //console.log('thisbb.top: ' + thisbb.top + ' thisbb.bottom: ' + thisbb.bottom + ' thisbb.left: ' + thisbb.left + ' thisbb.right: ' + thisbb.right);
-                    //console.log(prev);
-                    //console.log('prevbb.top: ' + prevbb.top + ' prevbb.bottom: ' + prevbb.bottom + ' prevbb.left: ' + prevbb.left + ' prevbb.right: ' + prevbb.right);
-
-                    //pos[1] = pos[1] + (prevbb.bottom - thisbb.bottom) + textOffset + (i*5);
-            }
-        }*/
-
-        
-        return "translate(" + pos + ")";
-
-    };
-}
-
-function styleTweenLabels(d) {
-    var interpolate = d3.interpolate(this._current, d);
-    return function (t) {
-        var d2 = interpolate(t);
-        return midAngle(d2) < Math.PI ? "start" : "end";
-    };
 }
